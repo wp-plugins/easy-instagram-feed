196530c157b69c6484d24c1b19c5cc10b30c7fab
priyanshu.mittal <priyanshu.mittal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-17 06:35:26 +0000

    0.1 version commited
    Added feature to fetch feeds from single user account.
    
    git-svn-id: https://plugins.svn.wordpress.org/easy-instagram-feed/trunk@1046675 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/optionpanel/general-settings.php
@@ -0,0 +1,41 @@
+<?php 
+
+   $eif_token_url = "https://instagram.com/oauth/authorize/?client_id=";
+    $eif_token_url .= '44a5744739304a48af362318108030bc';
+    $eif_token_url .= "&redirect_uri=http://webriti.com/easy-instagram-feed/eiflite/";
+   $eif_token_url .= "?return_uri=".admin_url('admin.php?page=easy-instagram-feed');
+    $eif_token_url .= "&response_type=token";
+
+?>
+
+<?php if(isset($_POST['submit'])){
+    $eif_settings = array();
+    $eif_settings = array(
+                    'eif_access_token' => $_POST['eif_access_token'],
+                    'eif_user_id' => $_POST['eif_user_id'],
+        );
+        
+        update_option('eif_settings',$eif_settings);
+
+}?>
+<form  name="eif_form" method="post"><?php $eif_settings = get_option('eif_settings'); ?>
+<h3>Authorize this app</h3>
+<div class="eif_auth_button">
+    <a href="<?php echo $eif_token_url;?>" class="button button-primary">Login to your account and authorize the app.</a>
+</div>
+<table class="form-table">
+        <tr valign="top">
+        <th scope="row"><label>Your Access Token </label></th>
+        <td><input type="text" id="eif_access_token"  name="eif_access_token" value="<?php esc_attr_e($eif_settings['eif_access_token']); ?>" /></td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row"><label>User Id</label></th>
+        <td><input type="text" id="eif_user_id" name="eif_user_id" value="<?php esc_attr_e($eif_settings['eif_user_id']); ?>" /></td>
+        </tr>
+       
+    </table>
+    
+ 
+    <input type="submit" name="submit" value="Submit" class="button button-primary"/>
+</form> 
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Easy Instagram Feed ===
+Contributors: priyanshu.mittal,a.ankit
+Donate link: http://www.webriti.com/
+Tags: instagram, instagram page, instagram feed, instagram photos,custom instagram feed,mobile instagram,
+Requires at least: 3.3+
+Tested up to: 4.0
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy Instagram Feed plugin allows to fetch the instagram feeds in your wordpress site. Just add the shortcode [easyinstagramfeed] in the normal wordpress page inorder to get the feeds.
+
+== Description ==
+
+Easy Instagram Feed plugin allows to fetch the instagram feeds in your wordpress site. Just add the shortcode [easyinstagramfeed] in the normal wordpress page inorder to get the feeds.
+
+Give **Easy Instagram Feed** a try. We are sure you will like it. 
+
+In case you face any problem, contact us via the [Forums](http://wordpress.org/support/plugin/easy-instagram-feed). 
+
+
+
+
+== Installation ==
+
+1. Download Easy Instagram Feed plugin.
+2. Upload the easy-instagram-feed folder to the /wp-content/plugins/ directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress and Enjoy.
+
+
+== Frequently Asked Questions ==
+
+
+
+
+== Screenshots ==
+
+1. Create instagram feed page like this
+
+
+== Changelog ==
+
+= 0.1 =
+This version provides basic functionality to authorize an app with the instagram and fetch instagram feeds with the help of shortcode [easyinstagramfeed] on pages.
