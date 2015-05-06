7d50dbacdea621933cc70c6294d99628e6e98bfc
priyanshu.mittal <priyanshu.mittal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 12:26:39 +0000

    added 1.5 version files
    git-svn-id: https://plugins.svn.wordpress.org/easy-instagram-feed/tags/1.5@1154492 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/optionpanel/display-feeds.php
@@ -0,0 +1,235 @@
+<div id="shortcode_atts" class = "postbox">
+        <div class="inside">
+            <div class="format-settings">
+                <div class="format-setting-wrap">
+    <form method = "post" action="#shortcode_atts">
+		<div class="format-setting-label">
+			<h3 class="label">Shortcode Attributes</h3>
+		</div>
+    </form>
+	<p><strong>How to use:</strong> [easyinstagramfeed userid="1591885187" backgroundcolor="#595e4d" num=5 post_type_photo='1' cols=4]</p>
+	 
+	<table id="menu-locations-table" class="widefat fixed">
+		<thead>
+		<tr>
+		<th class="wff-att-name" scope="col">Shortcode Attribute</th>
+		<th class="wff-att-name" scope="col">Value(for Eg)</th>
+		<th class="wff-att-uses" scope="col">Uses</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">userid</td>
+			<td class="wff-att-name">1591885187</td>
+			<td class="wff-att-uses">An Instagram User ID. Want to fetch feeds from multiple user accounts than specify their IDs and seperate them by commas. Use this tool to know your <a href="http://jelled.com/instagram/lookup-user-id" target="_blank">userid</a> . You can also get your id by authorizing this app in the General Settings of the Option Panel.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">accesstoken</td>
+			<td class="wff-att-name">1591885187.44a5744.385971946cc341fa9f84967c9ba1b9db</td>
+			<td class="wff-att-uses">An Instagram User accesstoken. Again you can get the token  by authorizing this app from the general settings.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">width</td>
+			<td class="wff-att-name">100</td>
+			<td class="wff-att-uses">The width of your feed in number. No need to add px or %. Default value is 100'.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">widthunit</td>
+			<td class="wff-att-name">% or px</td>
+			<td class="wff-att-uses">The width unit of the feed. Allowed units are px or %. Default value if not specified is %'.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">height</td>
+			<td class="wff-att-name">100</td>
+			<td class="wff-att-uses">The height of the feed. Just specify a number. Default value is 100..</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">heightunit</td>
+			<td class="wff-att-name">% or px</td>
+			<td class="wff-att-uses">The height unit of  feed. Use px or %.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">backgroundcolor</td>
+			<td class="wff-att-name">#595e4d</td>
+			<td class="wff-att-uses">The backgroundcolor color of your feed area. Default value #fff.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">orderby</td>
+			<td class="wff-att-name">random</td>
+			<td class="wff-att-uses">Sort Photos by order. It has two values <b>"newertoolder"</b> and <b>"random"</b>. Default order is <strong>newertoolder</strong> if not specified.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">num</td>
+			<td class="wff-att-name">5</td>
+			<td class="wff-att-uses">Limit number of images to show.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">cols</td>
+			<td class="wff-att-name">4</td>
+			<td class="wff-att-uses">Number of Column of feed. Default value is 4.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">resolution</td>
+			<td class="wff-att-name">low_resolution</td>
+			<td class="wff-att-uses">Resolution of images, has three values <b>thumbnail</b> , <b>low_resolution</b> and <b>standard_resolution</b>. Default resolution is low_resolution if not specified. Thumbnail will render image of size 150 * 150, low resolution 306 * 306 and standard 640px * 640px.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">imagepadding</td>
+			<td class="wff-att-name">5</td>
+			<td class="wff-att-uses">The image padding size in number.Default is 5.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">imagepaddingunit</td>
+			<td class="wff-att-name">px</td>
+			<td class="wff-att-uses">The image padding unit in % or px. Default is px.</td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">imageoverlay</td>
+			<td class="wff-att-name">yes</td>
+			<td class="wff-att-uses">Whether to show the 'overlay' on feed. 'yes' or 'no'. Default value yes.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">imageborder</td>
+			<td class="wff-att-name">yes</td>
+			<td class="wff-att-uses">Whether to show the 'Border' on feed. 'yes' or 'no'. Default value yes.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">imageshadow</td>
+			<td class="wff-att-name">yes</td>
+			<td class="wff-att-uses">Whether to show the 'image shadow' on feed. 'yes' or 'no'. Default value yes.</td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">loadmorebuttondisplay</td>
+			<td class="wff-att-name">yes</td>
+			<td class="wff-att-uses">Whether to show the 'Load More' button. 'yes' or 'no'. Default is no.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">loadmorebuttoncolor</td>
+			<td class="wff-att-name">#000000</td>
+			<td class="wff-att-uses">The Load More button color. Default is #000.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">loadmorebuttontextcolor</td>
+			<td class="wff-att-name">#fff</td>
+			<td class="wff-att-uses">The Load More button Text color. Default is #fff.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">loadmorebuttontext</td>
+			<td class="wff-att-name">Load More..</td>
+			<td class="wff-att-uses">The Load More button display text.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">followbuttondisplay</td>
+			<td class="wff-att-name">yes</td>
+			<td class="wff-att-uses">Whether to show the 'Follow on Instagram' button. 'yes' or 'no'. Defaut value is no.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">followbuttoncolor</td>
+			<td class="wff-att-name">#000000</td>
+			<td class="wff-att-uses">the Follow on Instagram button color. Dafault color is #000.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">followbuttontextcolor</td>
+			<td class="wff-att-name">#fff</td>
+			<td class="wff-att-uses">The Follow on Instagram button text color. Default is #fff.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">followbuttontext</td>
+			<td class="wff-att-name">left</td>
+			<td class="wff-att-uses">The Follow on Instagram button dispaly text.</td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">followbuttontext</td>
+			<td class="wff-att-name">Follo on Instagram..</td>
+			<td class="wff-att-uses">The Follow on Instagram button dispaly text.</strong></td>
+		</tr>
+		<tr>
+		<th class="wff-att-name" style="  background: darkgray;" colspan="3" scope="col">Available Attributes in pro version</th>
+		<th class="wff-att-name" scope="col"></th>
+		<th class="wff-att-name" scope="col"></th>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">searchtype</td>
+			<td class="wff-att-name">hashtag</td>
+			<td class="wff-att-uses">This is to specify which type of feed you want to display. If you want feed with hashtag then use value "hashtag" as a searchtype.If you want feeds from specific user accounts than use "user" as a value for searchtype.'); ?><br>
+		</strong></td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">hashtag</td>
+			<td class="wff-att-name">hashtag</td>
+			<td class="wff-att-uses">An Instagram Hastag. Want to fetch feeds from multiple Hastag than specify their Name and for multiple hastag seperate them by commas. Also no need to put # symbol before the hashtag name.</strong></td>
+		</tr>
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">showlightbox</td>
+			<td class="wff-att-name">yes or no</td>
+			<td class="wff-att-uses">The disable light-box in instagram midea you can set 'yes' or 'no' .Default is'yes'.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">captiondisplay</td>
+			<td class="wff-att-name">yes or no</td>
+			<td class="wff-att-uses">Whether to show the Caption on feed. 'yes' or 'no'. Defaut value is yes.</strong></td>
+		</tr>
+		
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">captiontextlength</td>
+			<td class="wff-att-name">10</td>
+			<td class="wff-att-uses">The Caption Text length. Default is 10.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">captiontextcolor</td>
+			<td class="wff-att-name">#666</td>
+			<td class="wff-att-uses">The Caption Text color. Default is #666.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">captiontextsize</td>
+			<td class="wff-att-name">14</td>
+			<td class="wff-att-uses">The Caption Text Size not need to metion px its bydefolut include in. Default is 14.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">headerdisplay</td>
+			<td class="wff-att-name">yes or no</td>
+			<td class="wff-att-uses">Whether to show the Header on feed. 'yes' or 'no'. Defaut value is yes.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">headertextcolor</td>
+			<td class="wff-att-name">#e34f0e</td>
+			<td class="wff-att-uses">The Header Text length. Default is #e34f0e.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">likesdisplay</td>
+			<td class="wff-att-name">yes or no</td>
+			<td class="wff-att-uses">Whether to show the Likes on feed. 'yes' or 'no'. Defaut value is yes.</strong></td>
+		</tr>
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">commentsdisplay</td>
+			<td class="wff-att-name">yes or no</td>
+			<td class="wff-att-uses">Whether to show the Comments on feed. 'yes' or 'no'. Defaut value is yes.</strong></td>
+		</tr>
+		
+		
+		<tr id="menu-locations-row">
+			<td class="wff-att-name">likescommentscolor</td>
+			<td class="wff-att-name">#dd9494</td>
+			<td class="wff-att-uses">The Likes And Comments  color. Default is #dd9494.</strong></td>
+		</tr>
+		
+		
+		
+		</tbody>
+	</table>	
+			   </div>
+            </div>
+        </div>
+	</div>
+	
